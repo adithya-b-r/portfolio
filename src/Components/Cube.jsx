52d3aad1631400479e00e3856cb2530665729732
Abr009 <bradithya945@gmail.com>
2025-06-27 10:42:15 +0530

    Fixed Cube issue and added About Section.

--- src/Components/Cube.jsx
+++ src/Components/Cube.jsx
@@ -28,8 +28,8 @@ const Cube = ({ ...props }) => {
   });
 
   return (
-    <Float floatIntensity={2}>
-      <group position={[9, -4, 0]} rotation={[2.6, 0.8, -1.8]} scale={0.74} dispose={null} {...props}>
+    <Float floatIntensity={1}>
+      <group position={[9, -6, 0]} rotation={[2.6, 0.8, -1.8]} scale={0.74} dispose={null} {...props}>
         <mesh
           ref={cubeRef}
           castShadow
--- src/Components/HeroCamera.jsx
+++ src/Components/HeroCamera.jsx
@@ -9,10 +9,10 @@ export const HeroCamera = ({ children, isMobile }) => {
     easing.damp3(state.camera.position, [0, 0, 20], 0.25, delta)
 
     if (!isMobile) {
-      easing.dampE(groupRef.current.rotation, [-state.pointer.y / 3, state.pointer.x / 5, 0], 0.25, delta)
+      easing.dampE(groupRef.current.rotation, [-state.pointer.y / 3, state.pointer.x / 3, 0], 0.25, delta)
     } else {
       if (isMobile) {
-        easing.dampE(groupRef.current.rotation, [0, state.pointer.x / 5, 0], 0.25, delta)
+        easing.dampE(groupRef.current.rotation, [0, state.pointer.x / 4, 0], 0.25, delta)
       }
     }
 
